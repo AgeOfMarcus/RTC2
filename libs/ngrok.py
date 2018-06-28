9606dc14eefa845455603b3a34bba50f7137d2d4
AgeOfMarcus <marcusjw.ftw@gmail.com>
2018-06-28 20:22:00 +0100

    Fixed ngrok get url

--- libs/ngrok.py
+++ libs/ngrok.py
@@ -21,7 +21,7 @@ def run_ngrok(service="http",port=1029):
         return get_url()
 
 def get_url():
-        os.system('echo $(curl -s localhost:4040/inspect/http | grep -oP \'window.common[^;]+\' | sed \'s/^[^\\(]*("//\' | sed \'s/")\\s*$//\' | sed \'s/\\"/"/g\') | jq -r ".Session.Tunnels | values | map(.URL) | .[]" | grep "^https:" > ngrok.url')
+        os.system('''echo $(curl -s localhost:4040/inspect/http | grep -oP 'window.common[^;]+' | sed 's/^[^\(]*("//' | sed 's/")\s*$//' | sed 's/\\"/"/g') | jq -r ".Session.Tunnels | values | map(.URL) | .[]" | grep "^https:"''')
         with open("ngrok.url","r") as f:
                 url = f.read()
         os.system("rm ngrok.url")
